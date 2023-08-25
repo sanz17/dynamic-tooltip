ea478aa8296a2945b8bc2e5dc8de3f904d82a234
sanz17 <kundusanhita13@gmail.com>
2023-08-25 16:02:26 +0530

    remove tooltip file

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,33 +1,44 @@
-import React, { useState } from 'react';
-import './Tooltip.css';
+// import React, { useState } from 'react';
+// import './Tooltip.css';
 
-function Tooltip({ buttonText, tooltipText, tooltipStyles }) {
-    const [showTooltip, setShowTooltip] = useState(false);
+// function Tooltip({ buttonText, tooltipText, tooltipStyles }) {
+//     const [showTooltip, setShowTooltip] = useState(false);
+//     const [tooltipStyles, setTooltipStyles] = useState({
+//         padding: '8px',
+//         backgroundColor: '#ff9900',
+//         color: '#fff'
+//     });
+//     const handleMouseEnter = () => {
+//         setShowTooltip(true);
+//       };
 
-    const handleMouseEnter = () => {
-        setShowTooltip(true);
-    };
+//       const handleMouseLeave = () => {
+//         setShowTooltip(false);
+//       };
 
-    const handleMouseLeave = () => {
-        setShowTooltip(false);
-    };
+//       const handleStyleChange = (property, value) => {
+//         setTooltipStyles(prevStyles => ({
+//           ...prevStyles,
+//           [property]: value
+//         }));
+//       };
 
-    return (
-        <div className="dynamic-tooltip-container">
-            <button
-                className="dynamic-tooltip-button"
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
-            >
-                {buttonText}
-            </button>
-            {showTooltip && (
-                <div className="dynamic-tooltip" style={tooltipStyles}>
-                    {tooltipText}
-                </div>
-            )}
-        </div>
-    );
-}
+//     return (
+//         <div className="dynamic-tooltip-container">
+//             <button
+//                 className="dynamic-tooltip-button"
+//                 onMouseEnter={handleMouseEnter}
+//                 onMouseLeave={handleMouseLeave}
+//             >
+//                 {buttonText}
+//             </button>
+//             {showTooltip && (
+//                 <div className="dynamic-tooltip" style={tooltipStyles}>
+//                     {tooltipText}
+//                 </div>
+//             )}
+//         </div>
+//     );
+// }
 
-export default Tooltip;
+// export default Tooltip;
